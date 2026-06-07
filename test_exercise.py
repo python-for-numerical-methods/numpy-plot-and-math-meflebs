88cb9b4f577d2f52ab094b2e3e79936c0a95cd9a
meflebs <meflebs@ac.sce.ac.il>
2026-06-07 22:47:33 +0300

    Update test_exercise.py

--- test_exercise.py
+++ test_exercise.py
@@ -1,17 +1,12 @@
 def factors(x):
+  f_list = []
+  for i in range(1, x + 1):
+    if x % i == 0:
+      f_list.append(i)
 
   return f_list
-
-     
-
 #check your code
 print(factors(6))
 print(factors(17))
 print(factors(24))
 print(factors(48))
-
-     
-[1, 2, 3, 6]
-[1, 17]
-[1, 2, 3, 4, 6, 8, 12, 24]
-[1, 2, 3, 4, 6, 8, 12, 16, 24, 48]
